ff11d2e56de8a6d253a04e7e5f1ebde8b5c3291a
wangze0618 <2867273828@qq.com>
2022-06-10 09:11:05 +0800

    proxy

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ module.exports = {
     open: true,
     proxy: {
       "^/app": {
-        ws: true, //是否启用websockets
+        pathRewrite: {
+          "^/api": "",
+        },
         changeOrigin: true, //开启代理： 在本地会创建一个虚拟服务端，然后发送请求数据，并且同时接收请求数据，这样客户端和服务端进行数据的交互就不会有跨域问题
         target: "http://api-toutiao-web.itheima.net/app", // 要访问的跨域的域名
       },
