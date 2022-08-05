bd19f996bcf2e0141de67789a67fcfb72c74778e
wangze0618 <2867273828@qq.com>
2022-08-05 17:50:14 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,31 +1,34 @@
 // 封装请求模块
-import axios from 'axios';
-import store from '../store/index'
-import JSONBig from 'json-bigint'
-// axios.defaults.baseURL = 'http://api-toutiao-web.itheima.net'
+import axios from "axios";
+import store from "../store/index";
+import JSONBig from "json-bigint";
+// axios.defaults.baseURL = "http://api-toutiao-web.itheima.net";
 const request = axios.create({
-    baseURL: 'http://api-toutiao-web.itheima.net/',
-    transformResponse: [function (data) {
-        try {
-            return JSONBig.parse(data)
-        } catch (error) {
-            return data
-        }
-    }]
-})
-
-// 请求拦截器
-request.interceptors.request.use(function (config) {
-    const user = store.state.user
-    if (user && user.token) {
-        config.headers.Authorization = `Bearer ${user.token}`
-    }
-    return config;
-}, error => {
-    return Promise.reject(error);
+  baseURL: "http://api-toutiao-web.itheima.net",
+  transformResponse: [
+    function (data) {
+      try {
+        return JSONBig.parse(data);
+      } catch (error) {
+        return data;
+      }
+    },
+  ],
 });
 
+// 请求拦截器
+request.interceptors.request.use(
+  function (config) {
+    const user = store.state.user;
+    if (user && user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // axios.defaults.headers = { Authorization: `Bearer ${store.state.user.token}` }
-export default request
-
+export default request;
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   devServer: {
     open: true,
-    proxy: {
-      "^/app": {
-        pathRewrite: {
-          "^/api": "/",
-        },
-        changeOrigin: true, //开启代理： 在本地会创建一个虚拟服务端，然后发送请求数据，并且同时接收请求数据，这样客户端和服务端进行数据的交互就不会有跨域问题
-        target: "http://api-toutiao-web.itheima.net/app", // 要访问的跨域的域名
-      },
-    },
+    // host: "localhost",
+    // proxy: "http://localhost:8080",
+    // public: "192.168.0.103:8080",
+    // proxy: {
+    //   "^/app": {
+    //     // pathRewrite: {
+    //     //   "^/api": "",
+    //     // },
+    //     changeOrigin: true, //开启代理： 在本地会创建一个虚拟服务端，然后发送请求数据，并且同时接收请求数据，这样客户端和服务端进行数据的交互就不会有跨域问题
+    //     target: "http://api-toutiao-web.itheima.net/", // 要访问的跨域的域名
+    //   },
+    // },
   },
 
   lintOnSave: false,
